b2138c00cdf6c9e39ff34e67b40fa73adc9faedf
unknown <louistr2706@gmail.com>
2021-10-16 06:51:30 +0700

    happy 2010

---
+++ main.js
@@ -0,0 +1,342 @@
+name = "";
+listname = [
+    // 1
+    "Phạm Nguyễn Hoàng Anh",
+    // 2
+    "Võ Như Bình",
+    // 3
+    "Nguyễn Hồ Khánh Duyên",
+    // 4
+    "Huỳnh Thị Kim Đan",
+    // 5
+    "Phan Thị Hoài Giang",
+    // 6
+    "Nguyễn Kim Thiên Hân",
+    // 7
+    "Nguyễn Phương Yên",
+    // 8
+    "Nguyễn Thị Hiền",
+    // 9
+    "Lê Thư Kỳ",
+    // 10
+    "Đặng Thị Kim Lành",
+    // 11
+    "Võ Thị Diễm Mi",
+    // 12
+    "Nguyễn Quang Trà My",
+    // 13
+    "Trần Thị Nga",
+    // 14
+    "Trần Thị Thảo Nguyên",
+    // 15 
+    "Trần Thị Thủy Tiên",
+    // 16
+    "Phạm Nguyễn Quỳnh Như",
+    // 17
+    "Đỗ Mỹ Oanh",
+    // 18
+    "Hoàng Thị Như Quỳnh",
+    // 19
+    "Đỗ Hồng Thảo Sương",
+    // 20
+    "Nguyễn Thùy Phương Thảo",
+    // 21
+    "Nguyễn Trần Anh Thư",
+    // 22
+    "Lê Hạnh Tiên",
+    // 23
+    "Trương Thị Thanh Trúc",
+    // 24
+    "Bùi Thị Xuân Uyên",
+    // 25
+    "Lê Hoàng Tuyết Vân",
+    // 26
+    "Dương Kiều Vy",
+    // 27
+    "Dương Nguyễn Ngọc Linh",
+    // 28
+    "Phạm Thị Loan",
+    // 29
+    "Quách Thị Minh Hiền",
+    // 30
+    "Bùi Thị Mai Trâm",
+    // 31
+    "Nguyễn Thị Thảo",
+    // 32
+    "Tôn Nữ Thùy Dương",
+    // 33
+    "Nguyễn Thị Chu Trinh",
+    // 34
+    "Nguyễn Ngọc Nhã Phương",
+    // 35
+    "Nguyễn Quế Trân"
+];
+
+index = 0;
+
+contentS = [];
+imglink = [];
+i = 0; 
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //1Phạm Nguyễn Hoàng Anh
+contentS[listname[i++]] = [
+    ["20/10 dui dẻ nha, sắp tới gặp nhiều hạnh phúc và tràn đầy sức sống","HayQuáTrời"], 
+    ["Chúc bà có một ngày lễ thiệt ấm áp mà không phải đóng MV 2 triệu năm","Đen Vâu"], 
+    ["Năm mới dui dẻ, sinh nhật dui dẻ, trung thu dui dẻ, giáng sinh dui dẻ, nói chung là vui cả năm","Ahihi"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //2Võ Như Bình
+contentS[listname[i++]] = [
+    ["Chúc các bạn nữ của toi(đùa thôi) xinh đẹp hơn, giỏi giang hơn, thành công hơn trong cuộc sống!","lời chúc từ đạt đz"], 
+    ["Bạn đã đối mặt với nghịch cảnh thông qua sự kiên cường và bạn đã giải quyết vấn đề của mình bằng một nụ cười và bạn đã che lấp những nỗi đau trong lòng. Tất cả những gì tôi muốn ở bạn là được hạnh phúc ngày hôm nay. Chúc mừng ngày Phụ nữ Việt Nam!","đạt"], 
+    ["Mọi thứ trước mắt đang chờ, hãy bước tiếp để khám phá nhé! 20/10 vui vẻ","Ahihi"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //3"Nguyễn Hồ Khánh Duyên",
+contentS[listname[i++]] = [
+    ["Chúc bạn, dù đang ở đâu, làm gì thì cũng thật hạnh phúc, vui vẻ với điều mình làm nhé. Hẹn mọi người lên trường gặp","Chàng trai hay làm khảo sát"], 
+    ["Một người phụ nữ duyên dáng nhận được sức mạnh từ những khó khăn, mỉm cười trong đau khổ và dường như còn mạnh mẽ hơn với những lời cầu nguyện và hi vọng. Chúc bạn một ngày Phụ nữ Việt Nam vui vẻ, khoan khoái vì hôm nay là ngày của bạn!",":))))"], 
+    ["Nhân ngày 8/3 + 12/7, mình chúc bạn có một niềm vui bất tận nhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","nha???"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //4Kim Đan
+contentS[listname[i++]] = [
+    ["Tôi mong bạn mãi xinh đẹp, luôn là bông hoa thơm trong vườn hoa ngát hương. Và mong bạn trên con đường kiếm tìm hạnh phúc sẽ tìm được một nửa của mình.","From Đạt with luv"], 
+    ["Con chim én màu xanh <br> Trên cành cây màu đỏ <br> Lại đây tui nói nhỏ <br>Chúc bạn mãi xinh nha","Quán Quân King Of Rap!!!"], 
+    ["Chúc những điều tốt đẹp đến với bạn như thứ mà cha mẹ đã trao cho bạn :3","Luvvv"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //5Hoài Giang
+contentS[listname[i++]] = [
+    ["Chúc các nữ lớp chúng ta thành công, có thật nhiều tiền(ko phải là tiền án đâu)","quangdat"], 
+    ["bạn là một cô gái độc nhất！<br>Hãy cảm thấy mình đặc biệt, duy nhất và hạnh phúc vào ngày của bạn nhé cô gái. Chúc bạn một 20/10 hạnh phúc!","đạt"], 
+    ["Chiều cao không cần phải e dè, vì đó là cute nên nhớ vui hết mình nhé","vui lên nào!!!"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //6ThiênHân
+contentS[listname[i++]] = [
+    ["Một ngày vui vẻ nhé! <br> 20/10 ngàn nụ cười:))","đạt đz"], 
+    ["chào một fan girl của harry porter！<br> Bạn là người bạn vô cùng thú vị và người bạn tuyệt vời của tôi. Tôi thật vinh hạnh. Chúc mừng ngày của bạn nhé!","Hân._."], 
+    ["20/10 chúc bạn mãi tươi vui, duyên dáng và yêu kiều nha, Hermione",":)))"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //7Phương Yên
+contentS[listname[i++]] = [
+    ["Chúc bạn mãi đẹp gái, Pass môn vèo vèo ♡♡♡","from đạt with luv!"], 
+    ["Chúc những dự định của bà sẽ luôn đi theo đúng con đường mà bà đó chọn chứ không bị lạc trôi về nơi phương xa ấy! ","Sơn Tường nhầm Sơn Tùng MTP"], 
+    ["Chúc chị Yên 20/10 vui tươi lấy sức để chạy deadline bài tập :v","Ziazia"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //8Nguyễn Thị Hiền
+contentS[listname[i++]] = [
+    ["Chúc bạn nhận được nhiều quà, nhiều hoa nhiều lời khen lời chúc của bọn tôi trong ngày hôm nay. Chúc bạn gặp nhiều may mắn hơn, hạnh phúc hơn nhiều niềm vui, nhiều điều tuyệt diệu hơn.","đạt triết gia"], 
+    ["Sóng bắt đầu từ gió<br> Gió bắt đầu từ đâu?<br> Tui cũng không biết nữa <br> Nhưng chúc bạn mãi xinh","from chúa tể gieo vần, thần đông záp ziệt!"], 
+    ["Luv 4 you!","from đạt triết gia withlove"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //9Thư Kỳ
+contentS[listname[i++]] = [
+    ["Ăn mau chóng lớn nhe 👍👍","bí idea quá giờ ko biết ghi gì"], 
+    ["Trăm năm núi đá cũng mòn<br> Cớ sao cậu lại vẫn còn trẻ trung <br> Ngàn năm nước biển chẳng còn <br> Cớ sao cậu lại xinh thêm thế này.","from chúa tể záp việt!"], 
+    ["Cuộc sống đâu ai biết trước được điều gì đâu, nên cứ 20/10 này thì ta cứ vui dù dài hay lâu","Ráp Diệt yeah yeah"], 
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //10Kim Lành
+contentS[listname[i++]] = [
+    ["Chúc bạn luôn thật xinh đệp, hạnh phúc và đạt được thành công như mong đợi của bản thân nha","đạt bét boi :))"], 
+    ["Có con mèo beo béo <br> Cuộn một cục tròn xoe <br>  Này cái cậu cute <br> 20 tháng 10 vui vẻ","đạt hay làm thơ!"], 
+    ["20/10 chúc bạn mãi cuteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","Nha!!!"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //11Diễm Mi
+contentS[listname[i++]] = [
+    ["Chúc bà có thể fly high với ước mơ của đời mình và tìm được ý trung nhân của đời mình và có một cuộc sống ấm no hạnh phúc nè!! ^^","content from my friends"], 
+    ["Xin chúc mọi điều tốt đẹp nhất đều dành cho bạn vào hôm nay, thật vui vẻ và hạnh phúc nhé ","Noname"], 
+    ["Nhân ngày 20 - 10, chúc bạn tôi luôn luôn xinh đẹp, tươi trẻ và thành công để có thêm nhiều chàng trai đeo bám như lửa bám xăng, như răng bám lợi, như trời bám mây, như cây bám đất, như sự cute bám bạn","thơ đỉnk ko"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //12Trà My
+contentS[listname[i++]] = [
+    ["Trăng lên đỉnh núi trăng tà <br> Còn bạn cứ mãi xink hoài vậy sao. <br> Chúc bạn mãi mãi giữ đc vẻ đẹp của mình như bài thơ của tui ở trên :)))))","a tờ at đờ át đát nặng đạt!"], 
+    ["You’ve faced adversity through resilience and you have resolved your problems by means of a smile and you’ve obscured your tribulations in your heart. Tui cx ko hiểu nó nói gì nhưng đại khái là chúc vui vẻ","đạt"], 
+    ["Đương đầu với thế giới, bức phá những điều mới, niềm vui tiếp nối tới, 20/10 phơi phới","Ráp Diệt"]
+];
+
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //13Thị Nga
+contentS[listname[i++]] = [
+    ["20/10 lại lặp lại rồi. Nhưng đâu năm nào giống năm nào. Mong rằng bé lun phát triển, ngày càng xinh đẹp, đạt được nhiều mục tiêu trong cuộc sống. Sống những ngày trọn vẹn nhaaaaaa !!!!","TQĐ"], 
+    ["Sắc đẹp, trí tuệ, tình yêu... tôi mong tất cả mọi thứ tốt đẹp sẽ đến với bé nhiều hơn từ hôm nay!!!","Noname"], 
+    ["Ngày 20/10, xin gửi đến các chị em đôi dòng ngắn gọn: chúc chị em vui như chim sẻ, khỏe như đại bàng, giàu sang như chim phượng, làm lụng như chim sâu, sống lâu như chim đà điểu.","xin được giấu tên"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //14Thảo Nguyên
+contentS[listname[i++]] = [
+    ["Chúc bạn ngày càng xinh đẹp, hạnh phúc nở nụ cười nhiều hơn, thuận lợi và thành công trên con đường mình chọn nhé! Là con gái là không những phải xinh mà phải trở nên quyền lực nữa đó nha hihi!","a tờ át đờ át đát nặng đạt"], 
+    ["Có vài đốm lửa nhỏ <br> Bỗng bùng cháy thật to <br>  Vẫn hỏi nhỏ câu này <br>  Sao mà bạn xinh thế <br>  Nói thêm tiếp một câu<br>  20 tháng 10 vui vẻ","chúa tể gieo thơ!"], 
+    ["ê bớt xink lại tí chứ xink vậy ai chịu nổi","đạt"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg";  //15thủy tiên
+contentS[listname[i++]] = [
+    ["Trăm năm trong cõi người ta, <br>Chữ tài chữ sắc là bạn chứ đâu. <br>Trải qua một cuộc bể dâu <br>Mà bạn vẫn giữ nét tiên lạ thường","đạt gút boi"], 
+    ["Chúc bạn nhận đư­ợc nhiều quà, nhiều hoa nhiều lời khen lời chúc của phái nam trong ngày hôm nay. Chúc bạn gặp nhiều may mắn hơn, hạnh phúc hơn nhiều niềm vui, nhiều điều tuyệt diệu hơn.","đạt"], 
+    ["Cầu một năm phát tài","Đ"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //16Quỳnh Như
+contentS[listname[i++]] = [
+    ["Every home, every heart, every feeling and every moment of happiness is truly incomplete without you. Only you can complete this world. Have a wonderful women's day!","đạt"], 
+    ["câu vừa rôi dịch gg đó chứ nqu lắm","chúa tể gg dịch"], 
+    ["1 2 3 con thỏ <br> nằm dưới gốc cây nhỏ <br>  oh cái bạn ciu te <br>  dễ thương dữ vậy ỏ <br> chúc 20/10 vui vẻ. ","záp diệt!"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //17Mỹ Oanh
+contentS[listname[i++]] = [
+    ["Làn thu thủy, nét xuân sơn <br> Hoa ghen thua thắm, liễu hờn kém xanh. <br>Bạn tui nghiêng nước nghiêng thành, <br>Sắc đành nhờ bạn, Toán nhờ bạn luôn.","Nguyễn Du nhầm Nguyễn Đạt"], 
+    ["Cuộc sống này, chỉ có làm, cần cù, chịu khó, siêng năng. Chỉ có làm, thì mới có ăn. Nhớ phải tìm anh ny nào biết nấu ăn đó nhe^^","chú tuân"], 
+    ["Nhất sinh nhị <br> Nhị sinh tam <br>Tam sinh vạn vật<br>bạn tôi đẹp quá<br>vạn vật hoá hư vô. ","chú tuân:))"]
+];
+
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //18Như Quỳnh
+contentS[listname[i++]] = [
+    ["Không biết nói gì cả nến bấm tiếp theo đi:))","Đạt hay quên"], 
+    ["Mau có thiệp hồng với bạn NMP nhé  :vvv","Noname"], 
+    ["chúc vui vẻ, mau giàu để mua sách cho toi","đạt chuyên 'cần'"],
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //19Thảo Sương"
+contentS[listname[i++]] = [
+    ["Chưa từng gặp nhưng nhân đây tôi xin gửi lời chúc sức khỏe và lời chào trân trọng nhất","ĐạtTrangTrọng"], 
+    ["Nhân dịp 20 tháng 10, xin chúc bạn: <br>Trẻ trung như heo sữa <br>Bốc lửa như heo quay <br>Hăng say như heo con <br>Sắc son như heo đất<br>  Đủ chất như...heo thịt<br> Chúc mừng, chúc mừng!!!","Đạt thực phẩm"], 
+    ["20 tháng 10 năm nay, tôi xin thay mặt Đảng, Nhà Nước và một nửa thế giới, chúc bạn một ngày nhiều niềm vui, hạnh phúc và có thật nhiều quà từ chàng trai của mình. Happy women's day!","Chrish"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //20Phương Thảo
+contentS[listname[i++]] = [
+    ["Nhân ngày Phụ nữ Việt Nam, tôi xin chúc chị em luôn tươi cười như hít phải khí N20, bay bổng như H2, bản lĩnh như N2, thăng hoa như L2 và hiền hậu như H2O. Happy women's day!","đạt"], 
+    ["Chúc bạn một ngày 20/10 ý nghĩa, vui tươi tràn ngập hạnh phúc!","QĐ"], 
+    ["Chúc năm mới nhiều tin vui thiệt vui",":)))"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //21Anh Thư
+contentS[listname[i++]] = [
+    ["Nhất sinh nhị <br> Nhị sinh tam <br>Tam sinh vạn vật bạn tôi đẹp quá vạn vật hoá hư vô.","đạt nhà văn"], 
+    ["Today, remember that all life spring from you. Look at the world and smile that without you, life would have been impossible. Enjoy your day to the fullest.","chúa tể gg"], 
+    ["câu này bí content quá bỏ qua nhen","đạt hay quên!"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //22Hạnh Tiên
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên mình xin chúc bạn được các bạn nam khác chúc những lời chúc tốt đẹp",":vvvvv"], 
+    ["ko nghĩ ra câu gì hết bỏ qua đi <br> Happy Viet Nam Women's Day! Chúc cô bạn xinh đẹp của mình có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của bạn sẽ thành công.","đạt nghiêm trang"], 
+    ["20/10 nếu đang ế, liên hệ tôi, tôi sẽ mặc kệ bạn:))","quangdat"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //23Thanh Trúc
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên mình xin chúc bạn được các bạn nam khác chúc những lời chúc tốt đẹp",":vvvvv"], 
+    ["mong rằng bạn sẽ tìm được một người họp với mình<br> Happy Viet Nam Women's Day! Chúc cô bạn xinh đẹp của mình có một ngày 20/10 vui vẻ. Chúc cho mọi dự định trong tương lai của bạn sẽ thành công.","đạt"], 
+    ["20/10 mong bạn sẽ nhận được nhiều lời chúc đặc biệt hơn từ người đặc biệt hơn cả tôi","quangdat"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //24Xuân Uyên
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên tôi sẽ chúc bạn vui như hoa",":vvvvv"], 
+    ["tôi ko thể chúc bạn xinh hơn đc vì bạn xink sẵn rồi <br> Happy Viet Nam Women's Day! Chúc bạn của mình có một ngày 20/10 vui vẻ, hạnh phúc nhé.","đạt IT"], 
+    ["20/10 chúc bạn hạnh phúc nhất vào ngày hôm nay hơn những gì tôi nghĩ","quangdatwithlove"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //25Tuyết Vân
+contentS[listname[i++]] = [
+    ["chúc bạn vui như từ thức, hạnh phúc hơn cả lối nhỏ của đen vâu, thông minh như gia cát lượng",":vvvvv"], 
+    ["越南妇女节快乐！ 10 月 20 日快乐，我美丽的朋友。不管你同意与否，我喜欢你，从今以后我会一直保护你。女孩 <br> Happy Viet Nam Women's Day! Chúc cô bạn xinh đẹp của mình có một ngày 20/10 vui vẻ. Chúc cậu có được tôi","Đạt"], 
+    ["câu trên dùng đỡ gg dịch nên không biết đúng không. Dù gì thì chúc bạn có một ngày thật hạnh phúc nhé, cô gái","luv 4 you"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //26Kiều Vy
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên mình xin chúc bạn được các bạn nam khác chúc những lời chúc tốt đẹp",":vvvvv"], 
+    ["be happy!! hãy là chính mình nhé cô gái<br> Happy Viet Nam Women's Day! Chúc cô bạn xinh đẹp của mình có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của bạn sẽ thành công.","đạt IT"], 
+    ["20/10 thật tuyệt vời nhé","love"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //27NgọcLinh
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên mình xin chúc bạn được các bạn nam khác chúc những lời chúc tốt đẹp",":vvvvv"], 
+    ["be fun!! hãy là chính mình nhé cô gái<br> Happy Viet Nam Women's Day! Chúc cô bạn xinh đẹp của mình có một ngày 20/10 vui vẻ, hạnh phúc nhé. Vạn sự như ý.","đạt IT"], 
+    ["20/10 thật tuyệt vời nhé","louis"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //28Phạm Thị Loan
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //29Quách Thị Minh Hiền
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt chuyên tin"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //30Bùi Thị Mai Trâm
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //31Nguyễn Thị Thảo
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //32Tôn Nữ Thùy Dương
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //33Nguyễn Thị Chu Trinh
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên em xin chúc cô những lời chúc tốt đẹp",":vvvvv"], 
+    ["chúc cô thật vui vẻ và hạnh phúc vào ngày này <br> Happy Viet Nam Women's Day! Chúc cô xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho mọi dự định trong tương lai của cô sẽ thành công.","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //34Nhã Phương
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên chúc bạn thật đẹp hơn nhen",":vvvvv"], 
+    ["chúc P thật vui vẻ và hạnh phúc vào ngày này <br> Chúc P xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho P giàu hơn để mua sách cho Đạt","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+imglink[listname[i]]="imgs/"+(i+1)+".jpg"; //35quếtrân
+contentS[listname[i++]] = [
+    ["Hôm nay là ngày phụ nữ nên chúc bạn thật đẹp hơn nhen",":vvvvv"], 
+    ["chúc T thật vui vẻ và hạnh phúc vào ngày này <br> Chúc T xinh đẹp của em có một ngày 20/10 vui vẻ, hạnh phúc nhé. Chúc cho T giàu hơn để mua sách cho Đạt","Đạt hsg"], 
+    ["20/10 ko biết nói gì hơn hêt rồi ạ.","đạt hsg"]
+];
+
+function btnClick() {
+    name = document.getElementById("name-girl").value;
+    var found = false;
+    index = 0;
+
+    listname.forEach(realname => {
+        if (realname==name) {
+            var contElement = document.getElementById("after-input");
+            contElement.style.display = "block";
+
+            var imggirl = document.getElementById("avatar");
+            imggirl.src = imglink[name];
+
+            var ct = document.getElementById("mess-ct");
+            ct.innerHTML = contentS[name][0][0];
+            var si = document.getElementById("mess-si");
+            si.innerHTML = "-From " + contentS[name][0][1] + " with love-"
+            var nextBtn = document.getElementById("btnNext");
+            nextBtn.innerHTML = "Tiếp theo (" + (index+1) + "/" + (contentS[name].length) +")";
+
+            found = true;
+        }
+    });
+
+    
+    if (!found) {
+        alert("Tên nhập không đúng rồi thử viết hoa chữ cái đầu xem :(((")
+    }
+
+    
+}
+
+function btnNextClick() {
+
+    if (index >= contentS[name].length-1) {
+        index = 0;
+    }
+    else {
+        index++;
+    }
+
+    var ct = document.getElementById("mess-ct");
+    ct.innerHTML = contentS[name][index][0];
+    var si = document.getElementById("mess-si");
+    si.innerHTML = "-From " + contentS[name][index][1] + " with love-";
+
+    var nextBtn = document.getElementById("btnNext");
+    nextBtn.innerHTML = "Tiếp theo (" + (index+1) + "/" + (contentS[name].length) +")";
+
+}
